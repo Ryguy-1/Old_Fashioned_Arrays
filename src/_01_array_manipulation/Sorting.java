55dde0e7e38ca9eba6e01f73ee3b95d65155d869
Ryguy-1 <league@WTS-iMac-11.attlocal.net>
2018-04-15 12:57:04 -0700

    q
    q

--- src/_01_array_manipulation/Sorting.java
+++ src/_01_array_manipulation/Sorting.java
@@ -2,4 +2,26 @@ package _01_array_manipulation;
 
 public class Sorting {
 
+	public static String[] sort(String[] testArray) {
+		int counter = 0;
+		String temp = "";
+		while (counter<=testArray.length*2) {
+			System.out.println("hi");
+		for (int i = 1; i < testArray.length; i++) {
+			if(testArray[i].compareTo(testArray[i-1])<0){
+				temp = testArray[i-1];
+				testArray[i-1]=testArray[i];
+				testArray[i]=temp;
+				temp = "";
+				counter+=1;
+			}
+		}
+		}
+		for (int i = 0; i < testArray.length; i++) {
+			System.out.println(testArray[i]);
+		}
+		
+		return testArray;
+	}
+
 }
