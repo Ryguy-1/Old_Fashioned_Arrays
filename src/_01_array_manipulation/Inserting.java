534053e37bc9d29caf45f3479904336e1258dfe2
Ryguy-1 <league@WTS7.attlocal.net>
2018-05-06 12:56:19 -0700

    done w/ 1 need to do 2

--- src/_01_array_manipulation/Inserting.java
+++ src/_01_array_manipulation/Inserting.java
@@ -3,5 +3,32 @@ package _01_array_manipulation;
 
 
 public class Inserting {
+
+	public static int[] insertAt(int[] testArray, int x, int y) {
+		int[] array = new int[testArray.length+1];
+		for (int i = 0; i < array.length; i++) {
+			if(i<x) {
+				array[i]=testArray[i];
+			}else if(i==x) {
+				array[i]=y;
+			}else {
+				array[i]=testArray[i-1];
+			}
+		}
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+		return array;
+	}
+
+	public static String[] insertAlphabetically(String[] orderedArray, String string) {
+		String[] array = new String[orderedArray.length+1];
+		for (int i = 0; i < orderedArray.length; i++) {
+			array[i]=orderedArray[i];
+		}
+		array[orderedArray.length]=string;
+		Sorting.sort(array);
+		return array;
+	}
 	
 }
--- src/_01_array_manipulation/Sorting.java
+++ src/_01_array_manipulation/Sorting.java
@@ -3,24 +3,16 @@ package _01_array_manipulation;
 public class Sorting {
 
 	public static String[] sort(String[] testArray) {
-		int counter = 0;
-		String temp = "";
-		while (counter<=testArray.length*2) {
-			System.out.println("hi");
-		for (int i = 1; i < testArray.length; i++) {
-			if(testArray[i].compareTo(testArray[i-1])<0){
-				temp = testArray[i-1];
-				testArray[i-1]=testArray[i];
-				testArray[i]=temp;
-				temp = "";
-				counter+=1;
+		for (int i = 0; i < testArray.length; i++) {
+			for (int j = i+1; j < testArray.length; j++) {
+				if(testArray[j].compareTo(testArray[i])<0) {
+					String x = testArray[i];
+					testArray[i]=testArray[j];
+					testArray[j]=x;
+					
+				}
 			}
 		}
-		}
-		for (int i = 0; i < testArray.length; i++) {
-			System.out.println(testArray[i]);
-		}
-		
 		return testArray;
 	}
 
