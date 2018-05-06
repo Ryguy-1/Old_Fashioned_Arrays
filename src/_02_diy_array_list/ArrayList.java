96ca0d840b4ebaf95b27d0a7e3bef6ac32b0b7be
Ryguy-1 <league@WTS7.attlocal.net>
2018-05-06 12:56:41 -0700

    ASD

--- src/_02_diy_array_list/ArrayList.java
+++ src/_02_diy_array_list/ArrayList.java
@@ -1,5 +1,17 @@
 package _02_diy_array_list;
 
-public class ArrayList{
+public class ArrayList<T>{
+	T[] array;
+	
 
+
+	ArrayList(){
+		array = (T[]) new Object();
+		
+	}
+	
+	void add(int x) {
+		
+		
+	}
 }
